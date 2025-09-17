69dcc33d71c4fcf49cbb2b3bcabf7485872ac0f6
Mohit Thakkar <thakkarmohit90@gmail.com>
2025-09-17 13:49:00 +0530

    disabled the rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,15 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off", 
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
